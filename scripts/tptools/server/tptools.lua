8927a25b6487d514071b004132daa752fd8485af
Nimphina <rune80@hotmail.co.uk>
2014-01-03 22:18:21 +0000

    updates on tpscript

---
+++ scripts/tptools/server/tptools.lua
@@ -0,0 +1,30 @@
+--JC2MP player tp
+
+-- Define the function.
+player_tp = function(args)
+		
+	local chat_string = args.text:split(" ")
+
+	if #chat_string >= 2 and string.lower(chat_string[1]) == "/tpp" then -- if string is greater or equal to two and contains /tpp
+
+		for player_ints in Server:GetPlayers() do -- Run through list of players on the server
+
+			if(player_ints:GetName() == chat_string[2]) then --if the player is the one we are looking for
+				Chat:Send(args.player, "Teleporting you to " .. player_ints:GetName(), Color(200, 100,3))
+				args.player:SetPosition(player_ints:GetPosition()) --Set player to the position of the player he wanted to tp to
+				return
+			end
+
+		end
+		return false
+	elseif #chat_string >= 2 and string.lower(chat_string[1]) == "/tpv" then
+
+		local new_player_vector = Vector3(tonumber(chat_string[2]), tonumber(chat_string[3]), tonumber(chat_string[4]))
+		Chat:Send(args.player, "Teleporting you to " .. tostring(new_player_vector), Color(200, 100,3))
+		args.player:SetPosition(new_player_vector)
+		return
+	end
+	return true
+end
+-- Subscribe our function to the PlayerChat event.
+Events:Subscribe("PlayerChat", player_tp)
